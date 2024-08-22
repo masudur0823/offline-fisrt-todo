beb6277d35605e01a69313de0e319ab7420a3165
masudur0823 <masudur0823@gmail.com>
2024-08-22 18:58:57 +0600

    r

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,39 +1,19 @@
-"use client";
-
 // import { useEffect } from "react";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Navigation from "./components/Navigation";
 import { ModeProvider } from "@/context/ModeProvider";
-import { useEffect } from "react";
 // import ReactQueryProvider from "@/context/ReactQueryProvider";
 
 const inter = Inter({ subsets: ["latin"] });
 
-// export const metadata = {
-//   title: "Offline-first-todo",
-//   description: "Offline first todo app",
-//   // manifest: "/manifest.json",
-// };
+export const metadata = {
+  title: "Offline-first-todo",
+  description: "Offline first todo app",
+  manifest: "/manifest.json",
+};
 
 export default function RootLayout({ children }) {
-  useEffect(() => {
-    if (typeof window !== "undefined" && "serviceWorker" in navigator) {
-      window.addEventListener("load", () => {
-        navigator.serviceWorker
-          .register("/service-worker.js")
-          .then((registration) => {
-            console.log(
-              "Service Worker registered with scope:",
-              registration.scope
-            );
-          })
-          .catch((error) => {
-            console.error("Service Worker registration failed:", error);
-          });
-      });
-    }
-  }, []);
   return (
     <html lang="en">
       <body className={inter.className}>
